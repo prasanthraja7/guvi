6d8e8cc8810cb36d15d55fba2c5a9d0ccff322b7
prasanthraja7 <prasanthraja135@gmail.com>
2017-06-07 12:18:59 +0530

    Add files via upload

---
+++ pv1.java
@@ -0,0 +1,20 @@
+package pkraja28;
+import java.util.*;
+public class pv1 {
+public static void main(String[] args){
+	int N;
+	Scanner p=new Scanner(System.in);
+	System.out.println("enter the integer");
+	N=p.nextInt();
+	
+	if(N<0)
+		System.out.println("negative");
+	else if(N>0)
+		System.out.println("positive");
+	else
+		System.out.println("zeroes");
+	
+	
+}
+
+}
