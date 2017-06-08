6a4f515e982f2eec75cfda5cdb503a6bd03c6db2
prasanthraja7 <prasanthraja135@gmail.com>
2017-06-08 09:46:10 +0530

    Add files via upload

---
+++ pv2.java
@@ -0,0 +1,16 @@
+package pkraja28;
+import java.util.*;
+public class pv2 {
+public static void main(String[] args){
+ int num;
+ Scanner in=new Scanner(System.in);
+ System.out.println("enter the number");
+ num=in.nextInt();
+ if(num%2==0){
+	 System.out.println("even number");}
+ else{
+	 System.out.println("odd number");
+ }
+}
+
+}
