e4a09a0d1fd7aa2dd145cbd39d237050d0d868f9
prasanthraja7 <prasanthraja135@gmail.com>
2017-07-10 16:08:30 +0530

    Create beginner2-1.java

---
+++ beginner2-1.java
@@ -0,0 +1,13 @@
+package level2;
+
+import java.util.Scanner;
+
+public class prcr11 {
+	public static void main(String []args){
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		System.out.println("The power of the number is:"+a*a);
+	}
+
+}
