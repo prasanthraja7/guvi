27676227471f8d39411a37d7b73dc7dc9d041f13
prasanthraja7 <prasanthraja135@gmail.com>
2017-07-10 17:21:22 +0530

    Create beginnerl6.java

---
+++ beginnerl6.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Oddnumbti
+{
+public static void main(String[] args)
+{
+int p,n;
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the value");
+p=in.nextInt();
+n=in.nextInt();
+for(int i=p;i<=n;i++)
+{
+if(i%2=0)
+{
+System.out.println(i);
+}
+}
+}
+}
